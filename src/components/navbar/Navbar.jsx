a956e8aa74b092f1d2b2e8f03adcdb0ebd21edfd
matiasecharri <matiasecharri@hotmail.com>
2022-12-25 06:43:16 -0300

    hay que dejar bien el nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import "./navbar.css";
 export default function Navbar() {
   return (
-   
-      <nav>
-        <button>Matias</button>
-        <button>Works</button>
-        <button>Skills</button>
-        <button>Contact</button>
-      </nav>
-   
+    <nav>
+      <button>Matias</button>
+      <button>Works</button>
+      <button>Skills</button>
+      <button>Contact</button>
+     
+    </nav>
   );
 }
