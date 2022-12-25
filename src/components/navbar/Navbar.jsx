faa027e063d8ccf299c6098d77d4c147e2e8f4bc
matiasecharri <matiasecharri@hotmail.com>
2022-12-25 11:09:47 -0300

    nav desaparece aparece

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
+
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav>
-      <button>Matias</button>
-      <button>Works</button>
-      <button>Skills</button>
-      <button>Contact</button>
-     
-    </nav>
+    <div>
+      <button className="opbuton" onClick={() => setIsOpen(!isOpen)}></button>
+      {isOpen && (
+        <nav>
+          <button>Matias</button>
+          <button>Works</button>
+          <button>Skills</button>
+          <button>Contact</button>
+        </nav>
+      )}
+    </div>
   );
 }
