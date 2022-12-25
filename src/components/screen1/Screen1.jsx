f27b33c2cbdc187060b890dda70ee73366b28796
matiasecharri <matiasecharri@hotmail.com>
2022-12-25 05:54:15 -0300

    up

--- src/components/screen1/Screen1.jsx
+++ src/components/screen1/Screen1.jsx
@@ -4,16 +4,22 @@ import "./screen1.css";
 export default function Screen1() {
   return (
     <>
+     <header>
+     <img src="https://media.discordapp.net/attachments/763661782555426817/1056489763844137060/pngwing.com_1.png" alt="" srcset="" />
+      </header>
       <main>
         <article>
           <h2>Hello, im a full-stack developer based in Buenos Aires.</h2>
         </article>
         <div className="containerMain">
           <div className="titular">
-          <h1>Matias Echarri</h1>
-          <p>Digital Artist (Developer, Designer, UI/UX) <br /> 
-<span>デジタルアーティスト</span></p>
+            <h1>Matias Echarri</h1>
+            <p>
+              Digital Artist (Developer, Designer, UI/UX) <br />
+              <span>デジタルアーティスト</span>
+            </p>
           </div>
+          <div className="aYdibella"></div>
         </div>
       </main>
     </>
