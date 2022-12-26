cb38994c210d8211020d04ce577747f3cf039784
matiasecharri <matiasecharri@hotmail.com>
2022-12-25 23:57:41 -0300

    agrego comentarios

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,12 +10,12 @@ export default function Navbar() {
   return (
     <div>
       <button
-        className={isOpen ? "opbuton" : "opbuton2"}
+        className={isOpen ? "opbuton" : "opbuton2"} //ACA ESTOY USANDO UN USESTATE QUE EMPIEZA EN FALSE, ES DECIR EMPIEZA SIN MOSTRAR NADA, EN CASO DE QUE SE  TOQUE EL BOTON SE ACTIVA EL SET IS OPEN Y MUESTRA LOS BOTONES DEL NAVBAR. ADEMÁS IF ISOPEN ES REAL, UTILIZA LA CLASE "OPBUTON QUE HACE UN BOTON GRANDE ROSADO, CASO CONTRARIO USA OPBUTON2 QUE ES UN BOTON CHICO"
         onClick={() => setIsOpen(!isOpen)}
       ></button>
       {isOpen && (
         <nav>
-          <button data-aos="fade-down" data-aos-duration="1200" >
+          <button data-aos="fade-down" data-aos-duration="1200">
             Me
           </button>
           <button data-aos="fade-down" data-aos-duration="1300">
