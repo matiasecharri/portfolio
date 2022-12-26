66087d9c2d262c947dc95c7172d4e04a49613e93
matiasecharri <matiasecharri@hotmail.com>
2022-12-25 23:22:05 -0300

    saco codigo sobrante

--- src/components/screen1/Screen1.jsx
+++ src/components/screen1/Screen1.jsx
@@ -24,6 +24,7 @@ export default function Screen1() {
           <div className="aYdibella"></div>
         </div>
       </main>
+      
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "../components/navbar/Navbar";
 import Screen1 from "../components/screen1/Screen1";
 
 export default function Home() {
