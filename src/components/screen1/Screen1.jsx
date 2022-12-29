a17971b50c59479ec447de7b2c945bf390df4a92
matiasecharri <matiasecharri@hotmail.com>
2022-12-29 06:06:36 -0300

    blender en pag

--- src/components/screen1/Screen1.jsx
+++ src/components/screen1/Screen1.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./screen1.css";
+import { Canvas } from "react-three-fiber";
+import { Michael } from "../Michael/Michael";
+import { AmbientLight } from "three";
+import {OrbitControls} from "@react-three/drei"
 
 export default function Screen1() {
   return (
     <>
-     <header >
-     <img  data-aos="fade-down" data-aos-duration="2000" src="https://media.discordapp.net/attachments/763661782555426817/1056550865827283074/ladoizquierdo.png" alt="" srcset=""  />
-     <img data-aos="fade-up" data-aos-duration="2500"  src="https://media.discordapp.net/attachments/763661782555426817/1056550866167025695/ladoderecho.png" alt="" srcset="" />
+      <header>
+        <div style= {{width: "100%", height: "30vh"}}>
+        <Canvas>
+         <ambientLight intensity={0.20}></ambientLight>
+         <pointLight position={[35,35,0]} intensity={0.4}/>
+         <pointLight position={[-35,35,0]} intensity={0.4}/>
+         <Suspense fallback={null}></Suspense>
+         <OrbitControls></OrbitControls>
+          <Michael></Michael>
+        </Canvas>
+        </div>
+        {/* <img  data-aos="fade-down" data-aos-duration="2000" src="https://media.discordapp.net/attachments/763661782555426817/1056550865827283074/ladoizquierdo.png" alt="" srcset=""  />
+     <img data-aos="fade-up" data-aos-duration="2500"  src="https://media.discordapp.net/attachments/763661782555426817/1056550866167025695/ladoderecho.png" alt="" srcset="" /> */}
       </header>
       <main>
-        
         <article>
           <h2>Hello, im a full-stack developer based in Buenos Aires.</h2>
         </article>
@@ -23,13 +36,8 @@ export default function Screen1() {
           </div>
           <div className="aYdibella"></div>
         </div>
-        <div className="containerMain2">
-          
-        </div>
-      
-      
+        <div className="containerMain2"></div>
       </main>
-      
     </>
   );
 }
